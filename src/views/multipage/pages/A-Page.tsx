72963917c3b5418066869561b01be7ba817c896f
Arif GEVENCI <gevenci.arif@gmail.com>
2025-11-16 12:08:29 +0300

    fix: page input label bug fix

--- src/views/multipage/pages/A-Page.tsx
+++ src/views/multipage/pages/A-Page.tsx
@@ -72,7 +72,7 @@ function A() {
             value={value.text}
             rows={4}
             className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-            placeholder="Enter your message"
+            placeholder="Enter your message (No record.)"
           />
         </div>
 
